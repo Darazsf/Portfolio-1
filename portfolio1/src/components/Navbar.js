99ac7d424003a0ed24fd17b87031d841003bff9a
Darazsf <darazs_ferenc@hotmail.com>
2023-07-01 22:05:37 +0200

    react-icon

--- portfolio1/src/components/Navbar.js
+++ portfolio1/src/components/Navbar.js
@@ -1,4 +1,5 @@
 import "../Css/navbar.css"
+import { TbMenu2 } from "react-icons/tb"
 
 export default function Navbar() {
 
@@ -9,7 +10,7 @@ export default function Navbar() {
             <div class="container-fluid">
                 <a class="navbar-brand" href="#"><img src="./_3f73c84f-270a-413b-8688-87a58db822ca.jpg" /></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+                    <span><TbMenu2/></span>
                 </button>
                 <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                     <ul class="navbar-nav me mb-2 mb-lg-0">
