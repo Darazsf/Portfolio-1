088754d56ec47be5732bb2563b53ef1af6672199
Darazsf <darazs_ferenc@hotmail.com>
2023-07-02 19:40:04 +0200

    main page

---
+++ portfolio1/src/components/Header.js
@@ -0,0 +1,16 @@
+import "../Css/header.css"
+
+export default function Header(){
+
+    return(
+        <div className="header-wrapper">
+            <div className="main-info">
+                <h1>
+                    Üdvözöllek az oldalamon!
+
+                </h1>
+            </div>
+
+        </div>
+    )
+}
