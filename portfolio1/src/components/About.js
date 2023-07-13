e7cfb08a91bccf34737a53d5e36605ebd1300a73
Darazsf <darazs_ferenc@hotmail.com>
2023-07-13 13:48:51 +0200

    upgrade

--- portfolio1/src/components/About.js
+++ portfolio1/src/components/About.js
@@ -10,7 +10,7 @@ export default function About(){
         <div className="valami">
           <h2 id="about">Rólam</h2>
             <p>Szia! Darázs Ferenc vagyok, .
-              Ez az oldal azért jött létre, hogy bemutathassam frontend fejlesztői kisebb projektjeim amiket az iskolában és szabadidőmben készítettem.
+              Ez az oldal azért jött létre, hogy bemutathassam kisebb projektjeim amiket az iskolában és szabadidőmben készítettem.
               Célom a képességeim fejlesztése és pályafutásom megkezdése.</p>
           </div>
           </div>
