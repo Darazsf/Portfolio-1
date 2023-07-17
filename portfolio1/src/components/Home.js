f134aed6f4ae3f0a32f831786bdbc8e9c0d7c251
Darazsf <darazs_ferenc@hotmail.com>
2023-07-17 20:52:19 +0200

    up

--- portfolio1/src/components/Home.js
+++ portfolio1/src/components/Home.js
@@ -2,28 +2,32 @@ import "../Css/home.css"
 import { TypeAnimation } from 'react-type-animation';
 
 
+
 export default function Home(){
 
     return(
-        <div className="header-wrapper">
-            <div className="main-info">
-                <h1>Üdvözöllek az oldalon!</h1>
-                <TypeAnimation
-        sequence={[
-          "A nevem Darázs Ferenc",
-          1000,
-       
-        ]}
-        speed={40}
-        repeat={2}
-        style={{ fontSize: '2em', color: "#DEB992" }}
-      />
-                <div class="wrap">
-                    <button class="button">Kontakt</button>
-                </div>
-                
-            </div>
 
-        </div>
+        <>
+<div id="home">
+            <div className="header-wrapper">
+                <div className="main-info">
+                    <h1>Üdvözöllek az oldalon!</h1>
+                    <TypeAnimation
+                        sequence={[
+                            "Üdvözöllek az oldalon!",
+                            1000,
+
+                        ]}
+                        speed={40}
+                        repeat={2}
+                        style={{ fontSize: '2em', color: "#323232" }}
+                    />
+                    
+
+
+                </div>
+            </div>
+            </div>
+        </>
     )
 }
--- portfolio1/src/components/Navbar.js
+++ portfolio1/src/components/Navbar.js
@@ -1,34 +1,39 @@
 import "../Css/navbar.css"
 import { TbMenu2 } from "react-icons/tb"
+import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
 
 export default function Navbar() {
 
 
     return (
-        <nav class="navbar navbar-expand-md bg-body-tertiary navbar sticky-top bg-body-tertiary " data-bs-theme="dark">
+        <nav class="navbar navbar-expand-xl bg-body-tertiary navbar sticky-top bg-body-tertiary min-vw-100 " data-bs-theme="dark">
             <div class="container-fluid">
-             <a class="navbar-brand" href="#"></a>
+             <a class="navbar-brand" href="#home">Darázs Ferenc</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span><TbMenu2/></span>
                 </button>
-                <div class="collapse navbar-collapse justify-content-end " id="navbarSupportedContent">
-                    <ul class="navbar-nav me mb-2 mb-lg-0 nav nav-underline ">
+                <div class="collapse navbar-collapse justify-content-sm-evenly " id="navbarSupportedContent">
+                    <ul class="navbar-nav me mb-2 mb-lg- nav nav-underline ">
                         <li class="nav-item">
-                            <a class="nav-link" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" aria-current="page" href="#home"><span>{"< "}</span>Home<span>{" />"}</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#about">Rólam</a>
+                            <a class="nav-link" href="#about"><span>{"< "}</span>Rólam<span>{" />"}</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#skills">Skills & CV</a>
+                            <a class="nav-link" href="#skills"><span>{"< "}</span>Skills & CV<span>{" />"}</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Munkáim</a>
+                            <a class="nav-link" href="#"><span>{"< "}</span>Munkáim<span>{" />"}</span></a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Kapcsolat</a>
+                            <a class="nav-link" href="#"><span>{"< "}</span>Kapcsolat<span>{" />"}</span></a>
                         </li>
+
                     </ul>
+                
+             
 
                 </div>
                 </div>
