22967ea3d7d10e1c6ba6699668c99cb0597c7e66
Darazsf <darazs_ferenc@hotmail.com>
2023-07-20 23:17:33 +0200

    correction

--- portfolio1/src/index.js
+++ portfolio1/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './components/Home';
+import Home from './pages/Home';
 import About from './pages/About';
 import Skills from './components/Skills';
 import Projects from './pages/Projects';
+
 
 
 
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     <>
+        
         <Home />
         <About />
         <Skills />
---
+++ portfolio1/src/pages/Home.js
@@ -0,0 +1,36 @@
+import "../Css/home.css"
+import { TypeAnimation } from 'react-type-animation';
+import Navbar from "../components/Navbar";
+
+
+
+
+export default function Home(){
+
+    return(
+
+        <>
+            <Navbar/>
+            <div id="home">
+                <div className="header-wrapper">
+                    <div className="main-info">
+                        <h1>Üdvözöllek az oldalon!</h1>
+                        <TypeAnimation
+                            sequence={[
+                                "Üdvözöllek az oldalon!",
+                                1000,
+
+                            ]}
+                            speed={40}
+                            repeat={2}
+                            style={{ fontSize: '2em', color: "#323232" }}
+                        />
+
+
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
