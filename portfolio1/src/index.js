ef0fb7694e20cb1173cfbb30d7e97ff37bff4458
Darazsf <darazs_ferenc@hotmail.com>
2023-07-20 23:10:34 +0200

    change-nav

--- portfolio1/src/index.js
+++ portfolio1/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './pages/About';
 import Skills from './components/Skills';
@@ -12,7 +11,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
     <>
-        <Navbar />
         <Home />
         <About />
         <Skills />
